4ddeb5ec9d72909a80d1cbf45e401e8369633bdc
Samuel Simard <112632053+SimSam2233346@users.noreply.github.com>
2023-03-24 15:08:50 -0400

    Add files via upload

---
+++ Prog 2 Fin Session/AmitieNamespace/Chien.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChienNamespace
+{
+    public class Chien
+    {
+        private string nomChien;
+        private string humeurChien;
+        private string amiChien;
+
+        //public Chien(string nomChien, string humeurChien,string amiDog)
+        //{
+        //    this.nomChien = nomChien;
+        //    this.humeurChien = humeurChien;
+        //    this.amiChien = amiDog;
+        //}
+
+        public Chien(string nomChien)
+        {
+            this.nomChien = nomChien;
+        }
+
+        public string GetNomChien() { return nomChien; }
+
+        public string GetAmiChien() { return amiChien; }
+        public void SetAmiChien(string amiChien) { this.amiChien = amiChien; }
+
+        public string GetHumeurChien() { return humeurChien;}
+        public void SetHumeurChien(string humeur) { this.humeurChien = humeur; }
+    }
+}
---
+++ Prog 2 Fin Session/AmitieNamespace/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatNamespace;
+using ChienNamespace;
+
+namespace AmitieNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int nbreHumeurChat = random.Next(0, 2);
+            bool chatPeutAvoirAmi;
+            bool amiDoggy = false;
+
+            Chat chat = new Chat("Cat");
+            Chien dog = new Chien("Dog");
+            Chien doggy = new Chien("Doggy");
+
+            dog.SetHumeurChien("Mauvaise");
+            doggy.SetHumeurChien("Mauvaise");
+            chat.SetAmiCat("Aucun");
+            dog.SetAmiChien("Aucun");
+            doggy.SetAmiChien("Aucun");
+
+            if (nbreHumeurChat == 0) { chat.SetHumeurChat("Mauvaise"); chatPeutAvoirAmi = false; }
+            else { chat.SetHumeurChat("Bonne"); chatPeutAvoirAmi = true; }
+
+            Console.WriteLine($"Nom Chat: {chat.GetNomChat()}\nHumeur Cat: {chat.GetHumeur()}\nMeilleur Ami Cat: {chat.GetAmiCat()}\n");
+            Console.WriteLine($"Nom Chien: {dog.GetNomChien()}\nHumeur Dog: {dog.GetHumeurChien()}\nMeilleur Ami Dog: {dog.GetAmiChien()}\n");
+            Console.WriteLine($"Nom Chien: {doggy.GetNomChien()}\nHumeur Doggy: {doggy.GetHumeurChien()}\nMeilleur Ami Doggy: {doggy.GetAmiChien()}\n");
+            Console.WriteLine("--------------------------------------------------------------------------\n");
+
+            if (chatPeutAvoirAmi == true) { chat.SetAmiCat("Dog"); dog.SetAmiChien("Cat"); dog.SetHumeurChien("Bonne"); }
+            else { chat.SetAmiCat("Aucun"); dog.SetAmiChien("Aucun"); }
+
+            Console.WriteLine($"Nom Chat: {chat.GetNomChat()}\nHumeur Cat: {chat.GetHumeur()}\nMeilleur Ami Cat: {chat.GetAmiCat()}\n");
+            Console.WriteLine($"Nom Chien: {dog.GetNomChien()}\nHumeur Dog: {dog.GetHumeurChien()}\nMeilleur Ami Dog: {dog.GetAmiChien()}\n");
+            Console.WriteLine($"Nom Chien: {doggy.GetNomChien()}\nHumeur Doggy: {doggy.GetHumeurChien()}\nMeilleur Ami Doggy: {doggy.GetAmiChien()}\n");
+            Console.WriteLine("--------------------------------------------------------------------------\n");
+
+            if (amiDoggy == false) { doggy.SetHumeurChien("Mauvaise"); }
+            else { doggy.SetHumeurChien("Bonne"); }
+
+            if (amiDoggy == false) { chat.SetAmiCat("Aucun"); chat.SetHumeurChat("Mauvaise"); dog.SetAmiChien("Doggy"); doggy.SetAmiChien("Dog"); amiDoggy = true; doggy.SetHumeurChien("Bonne"); }
+            else { doggy.SetAmiChien("Aucun"); }
+
+            //if (nbreHumeurChat == 0) { humeurChat = "Mauvaise"; chatPeutAvoirAmi = false; }
+            //else { humeurChat = "Bonne"; chatPeutAvoirAmi = true; }
+
+            //if (chatPeutAvoirAmi == true) { amiChat = "Dog"; amiChien = "Cat"; }
+            //else { amiChat = "Aucun"; amiChien = "Aucun"; }
+
+            //if (amiDoggy == false) { humeurDoggy = "Mauvaise"; }
+            //else { humeurDoggy = "Bonne"; }
+
+            //if (amiDoggy == false) { amiChat = "Aucun"; amiChien = "Doggy"; amiChien2 = "Dog"; amiDoggy = true; humeurDoggy = "Bonne"; }
+            //else { amiChien2 = "Aucun"; }
+
+            //Chat chat = new Chat("Cat", humeurChat, amiChat);
+            //Chien dog = new Chien("Dog", humeurDog, amiChien);
+            //Chien doggy = new Chien("Doggy", humeurDoggy, amiChien2);
+
+
+            Console.WriteLine($"Nom Chat: {chat.GetNomChat()}\nHumeur Cat: {chat.GetHumeur()}\nMeilleur Ami Cat: {chat.GetAmiCat()}\n");
+            Console.WriteLine($"Nom Chien: {dog.GetNomChien()}\nHumeur Dog: {dog.GetHumeurChien()}\nMeilleur Ami Dog: {dog.GetAmiChien()}\n");
+            Console.WriteLine($"Nom Chien: {doggy.GetNomChien()}\nHumeur Doggy: {doggy.GetHumeurChien()}\nMeilleur Ami Doggy: {doggy.GetAmiChien()}\n");
+            Console.WriteLine("--------------------------------------------------------------------------\n");
+        }
+    }
+}
